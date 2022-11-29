9a04e043081959c01580c589b18222a9d5e84db6
Александр Лёвин <maverick132@rambler.ru>
2022-11-29 18:36:26 +0300

    Rework

--- RandomExceptionsClass.java
+++ RandomExceptionsClass.java
@@ -1,16 +1,17 @@
 package java1.lessonsforqa.lesson1;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.LinkedList;
 
 public class RandomExceptionsClass {
     public static void main(String[] args) throws IOException {
         callMethod6("");
         ArrayList<String> stringArraysList = (ArrayList<String>) callMethod1();
-        int a=10, b=0;
+//        int a = 10, b = 0;
+        int a = 10, b = 1;
         callMethod6("");
         int div = callMethod2(a, b);
         System.out.println(div);
@@ -24,22 +25,34 @@ public class RandomExceptionsClass {
 
     private static void callMethod3(ArrayList<String> stringArraysList) {
         callMethod6("");
-        callMethod3(stringArraysList);
+//        callMethod3(stringArraysList);
     }
 
     private static Collection<String> callMethod1() throws IOException {
-        callMethod2(100000000, 10-10);
-        return new LinkedList<>();
+//        callMethod2(100000000, 10 - 10);
+        callMethod2(100000000, 10 - 9);
+        return new ArrayList<>();
     }
 
     public static int callMethod2(int a, int b) throws IOException {
         callMethod6("");
-        FileInputStream fis = new FileInputStream("1.txt");
-        fis.read();
 
-        if(fis.available() > 0) throw new RuntimeException();
-        int num = callMethod4("124O");
-        return a/b;
+        File file = new File("1.txt");
+        if (file.exists()) {
+            FileInputStream fis = new FileInputStream("1.txt");
+            fis.read();
+            if (fis.available() > 0) throw new RuntimeException();
+        } else {
+            System.out.println("File no found");
+        }
+
+        int num = callMethod4("1240");
+        if (b != 0) {
+            return a/b;
+        }
+        else {
+            throw new RuntimeException("Делить на 0 нельзя.");
+        }
     }
 
     private static int callMethod4(String s) {
@@ -50,7 +63,7 @@ public class RandomExceptionsClass {
     private static void callMethod5(String s) {
         callMethod6("");
         String[] strings = new String[5];
-        for (int i = 1; i <= strings.length; i++){
+        for (int i = 1; i < strings.length; i++) {
             strings[i] = s;
         }
     }
